ac66570cc290c6e96e649489aee064a9fd36f6c6
Nonamed <123anice@mail.ru>
2019-01-24 02:11:17 +0300

    Replace  file deploy.sh

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -3,14 +3,5 @@ set -e
 cd ~
 git clone https://github.com/Otus-DevOps-2017-11/reddit.git
 cd ~/reddit
-source /usr/local/rvm/scripts/rvm
-rvm requirements
-rvm use 2.4.1 --default
 bundle install
-puma -d
-sudo mv /tmp/run.service /lib/systemd/system/run.service
-sudo mv /tmp/script.sh /usr/local/bin/script.sh
-sudo chmod 644 /lib/systemd/system/run.service
-sudo systemctl daemon-reload
-sudo systemctl enable run.service
-sudo chmod u+x /usr/local/bin/script.sh
+mv /tmp/run.service /lib/systemd/system/run.service
